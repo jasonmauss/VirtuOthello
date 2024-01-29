05cb869174650f9c436a0502e6d56b3f25fa7399
Jason Mauss <jason.mauss@gmail.com>
2024-01-28 23:29:26 -0700

    reorganizing object responsibilities

--- web_typescript/build/gameTypeEnum.js
+++ web_typescript/build/gameTypeEnum.js
@@ -1,11 +1,11 @@
 import * as constants from "./constants.js";
 // This enum defines the four different types of games supported 
-export var newGameType;
-(function (newGameType) {
-    newGameType[newGameType["humanVsHuman"] = constants.GAME_TYPE_HUMAN_VS_HUMAN] = "humanVsHuman";
-    newGameType[newGameType["youAsBlack"] = constants.GAME_TYPE_YOU_AS_BLACK] = "youAsBlack";
-    newGameType[newGameType["youAsWhite"] = constants.GAME_TYPE_YOU_AS_WHITE] = "youAsWhite";
-    newGameType[newGameType["selfplay"] = constants.GAME_TYPE_SELF_PLAY] = "selfplay";
-})(newGameType || (newGameType = {}));
+export var gameType;
+(function (gameType) {
+    gameType[gameType["humanVsHuman"] = constants.GAME_TYPE_HUMAN_VS_HUMAN] = "humanVsHuman";
+    gameType[gameType["youAsBlack"] = constants.GAME_TYPE_YOU_AS_BLACK] = "youAsBlack";
+    gameType[gameType["youAsWhite"] = constants.GAME_TYPE_YOU_AS_WHITE] = "youAsWhite";
+    gameType[gameType["selfplay"] = constants.GAME_TYPE_SELF_PLAY] = "selfplay";
+})(gameType || (gameType = {}));
 ;
 //# sourceMappingURL=gameTypeEnum.js.map
