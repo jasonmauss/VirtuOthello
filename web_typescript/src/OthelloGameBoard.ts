82381e2b69ba8e43f2a62e7aca3fd2f6ce9a21ba
Jason Mauss <jason.mauss@gmail.com>
2024-01-29 23:03:51 -0700

    organizing object properties and methods

--- web_typescript/src/OthelloGameBoard.ts
+++ web_typescript/src/OthelloGameBoard.ts
@@ -10,9 +10,13 @@ export class OthelloGameBoard {
     }
 
     /**
-        performMove
-        movePlayed: instance of an OthelloGameMovePlayed class that defines the type of move
-        and on which square on the board
+     * Adds a piece to the board
+     * @remarks
+     * Accepts an OthelloGameMovePlayed object and uses its
+     * properties to determine where to add a piece to the board at.
+     *  
+     * @param movePlayed - instance of an OthelloGameMovePlayed class that defines the type of move
+     * and on which square on the board
     */
     public performMove(movePlayed:OthelloGameMovePlayed) {
 
@@ -26,6 +30,36 @@ export class OthelloGameBoard {
         this.AddMoveToLog(movePlayed);
     }
 
+    /**
+     * Clears all pieces from the board
+     * 
+     */
+    public clear() {
+        const board = document.getElementById(constants.CSS_ELEMENT_ID_BOARD);
+        // only get children that have a class name applied to them since those are
+        // the only ones we need to clear
+        const boardElements: NodeListOf<Element> = 
+            board?.querySelectorAll('.' + constants.CSS_CLASS_NAME_BLACK + ',' +
+                                    '.' + constants.CSS_CLASS_NAME_WHITE + ',' +
+                                    '.' + constants.CSS_CLASS_NAME_PLAYABLE + ',' +
+                                    '.' + constants.CSS_CLASS_NAME_MOST_RECENT_MOVE
+                                    ) as NodeListOf<Element>;
+                                    
+        for(let divElement of boardElements) {
+            divElement.classList.remove(constants.CSS_CLASS_NAME_WHITE);
+            divElement.classList.remove(constants.CSS_CLASS_NAME_BLACK);
+            divElement.classList.remove(constants.CSS_CLASS_NAME_PLAYABLE);
+            divElement.classList.remove(constants.CSS_CLASS_NAME_MOST_RECENT_MOVE);
+        }
+    }
+
+    /**
+     * Adds a move to the moves log for the game
+     * 
+     * @param movePlayed - instance of an OthelloGameMovePlayed class that defines
+     * the type of move played.
+     * 
+     */
     public AddMoveToLog(movePlayed: OthelloGameMovePlayed) {
         const playerColor:string = movePlayed.moveType === moveType.BlackPiece ? 'Black' : 'White';
         const optionText:string = `${playerColor} played at position ${movePlayed.position.toUpperCase()}`;
