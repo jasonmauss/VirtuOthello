ede60b3e603d405130a2a379e0826096416a0950
Jason Mauss <jason.mauss@gmail.com>
2024-01-30 23:30:57 -0700

    still organizing code and object responsibilities

--- web_typescript/build/index.js
+++ web_typescript/build/index.js
@@ -1,10 +1,11 @@
 import { OthelloGame } from "./OthelloGame.js";
 import { gameType } from "./gameTypeEnum.js";
 import * as constants from "./constants.js";
-const _othelloGame = new OthelloGame(gameType.humanVsHuman);
+let _othelloGame = new OthelloGame(gameType.humanVsHuman);
 // The startNewGame method is a method that can be called to
 // initiaite a new game
 const startNewGame = (game) => {
+    _othelloGame = game;
     switch (game.gameType) {
         case gameType.humanVsHuman:
             newHVHGame();
@@ -25,44 +26,24 @@ const startNewGame = (game) => {
 const performAllNewGameActions = () => {
     _othelloGame.gameBoard.clear();
     _othelloGame.clearMovesPlayed();
-    initializeNewGameBoard();
-};
-const performInitialBlackPieceMove = () => {
-    document.getElementById('c4')?.classList.add(constants.CSS_CLASS_NAME_BLACK);
-    document.getElementById('d4')?.classList.remove(constants.CSS_CLASS_NAME_WHITE);
-    document.getElementById('d4')?.classList.add(constants.CSS_CLASS_NAME_BLACK);
-    // 
-};
-const performInitialWhitePieceMove = () => {
-    document.getElementById('c5')?.classList.add(constants.CSS_CLASS_NAME_WHITE);
-    document.getElementById('d5')?.classList.remove(constants.CSS_CLASS_NAME_BLACK);
-    document.getElementById('d5')?.classList.add(constants.CSS_CLASS_NAME_WHITE);
-};
-const initializeNewGameBoard = () => {
-    const blackElementOne = document.getElementById('e4')?.classList.add(constants.CSS_CLASS_NAME_BLACK);
-    const blackElementTwo = document.getElementById('d5')?.classList.add(constants.CSS_CLASS_NAME_BLACK);
-    const whiteElementOne = document.getElementById('d4')?.classList.add(constants.CSS_CLASS_NAME_WHITE);
-    const whiteElementTwo = document.getElementById('e5')?.classList.add(constants.CSS_CLASS_NAME_WHITE);
-};
-const displayPlayableIndicators = () => {
+    _othelloGame.gameBoard.initializeNewGame();
 };
 const newHVHGame = () => {
     console.log('new Game Human vs Human');
-    performAllNewGameActions();
+    _othelloGame.performAllNewGameActions();
 };
 const newYABGame = () => {
     console.log('new Game You as Black');
-    performAllNewGameActions();
+    _othelloGame.performAllNewGameActions();
 };
 const newYAWGame = () => {
     console.log('new Game You as White');
-    performAllNewGameActions();
-    performInitialBlackPieceMove();
-    displayPlayableIndicators();
+    _othelloGame.performAllNewGameActions();
+    _othelloGame.performInitialBlackPieceMove();
 };
 const newSPLGame = () => {
     console.log('new Game Selfplay');
-    performAllNewGameActions();
+    _othelloGame.performAllNewGameActions();
 };
 const newGameHumanVsHumanClickHandler = (event) => {
     const game = new OthelloGame(gameType.humanVsHuman);
