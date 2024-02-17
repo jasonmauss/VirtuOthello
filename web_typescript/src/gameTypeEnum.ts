df959b4ce49e3a7ec613fcb3fa9d3ce8d45609b4
Jason Mauss <jason.mauss@gmail.com>
2024-02-16 22:46:17 -0700

    updated method comments + removed commented code

--- web_typescript/src/gameTypeEnum.ts
+++ web_typescript/src/gameTypeEnum.ts
@@ -2,7 +2,7 @@ import * as constants from "./constants.js"
 
 // This enum defines the four different types of games supported 
 export enum gameType {
-    humanVsHuman = 'HumanVsHuman', //constants.GAME_TYPE_HUMAN_VS_HUMAN,
+    humanVsHuman = 'HumanVsHuman',
     youAsBlack = 'YouAsBlack',
     youAsWhite = 'YouAsWhite',
     selfplay = 'SelfPlay'
